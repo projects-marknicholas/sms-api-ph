5326674e3723bd77b3a1e3d89c55f6b99b3ee458
projects-marknicholas <prograzon@gmail.com>
2026-01-19 16:29:24 +0800

    second commit

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ require('dotenv').config();
 // ===============================
 // FIREBASE ADMIN INIT
 // ===============================
-const serviceAccount = require('./firebase-admin.json');
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+  })
 });
 
 const db = admin.firestore();
